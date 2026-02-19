4233ef697abb7d23691ca03b3e819225f5ed9cba
alax3723 <alva.m.axelsson@gmail.com>
2026-02-19 16:16:35 +0100

    everything added

--- graph_traverse.js
+++ graph_traverse.js
@@ -1,30 +1,66 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var list_1 = require("./lib/list");
-var story_graph = {
+var prompSync = require("prompt-sync");
+var prompt = prompSync();
+var game_graph = {
     adj: [(0, list_1.list)(1, 2),
-        (0, list_1.list)(3, 4),
-        (0, list_1.list)(5),
-        (0, list_1.list)(6),
-        (0, list_1.list)(6),
         (0, list_1.list)(),
         (0, list_1.list)()],
-    size: 7
+    size: 3
 };
-var story_choice = ["path 1", "path 2"];
-function treverse_graph(graph) {
-    var nodes = graph.size;
-    var adj_lst = graph.adj;
-    var visited = new Array(nodes).fill(0);
-    // 
-    for (var i = 0; i < nodes; i++) {
-        if (adj_lst[i] === null) { // if a node have no children, we are done
-            return visited;
+var game_story = ["Vart vill du gå?", "Du överlevde!", "Du dog..."];
+var game_options = [["Sjön", "Huset"]];
+var game_test = {
+    graph: game_graph,
+    options: game_options,
+    story: game_story
+};
+// Psudo code
+function main(game) {
+    console.log("Welcome to game, do you want to start? y/n");
+    var input = prompt("> ");
+    if (input === "y") { // input to start game
+        play_game(game);
+    }
+    else { } // else do nothing?
+}
+function play_game(game) {
+    var current_node = 0;
+    for (var i = 0; i < game.graph.size; i++) { // loop over the grap
+        var current_options = game.options[current_node];
+        if ((0, list_1.is_null)(game.graph.adj[current_node])) { // if a node is an end-node, we end the game
+            console.log(game.story[current_node]);
+            game_over(game);
+            break;
         }
-        else if (visited[i] === 0) { // when visiting a node, change it to 1 in the array
-            visited[i] = 1;
+        else {
+            console.log(game.story[current_node]);
+            display_options(current_options);
+            var input = Number(prompt("> "));
+            if (input <= (0, list_1.length)(game.graph.adj[current_node]) + 1 && input > 0) {
+                console.log("You chose option " + input);
+                current_node = Number((0, list_1.list_ref)(game.graph.adj[current_node], input - 1));
+            }
+            else {
+                console.log("Invalid input! Choose again:");
+            }
         }
-        else { }
     }
 }
-console.log(treverse_graph(story_graph));
+function display_options(node_option) {
+    // displays the options to a story prompt
+    for (var i = 0; i < (node_option.length); i++) {
+        console.log("Option " + (i + 1) + ": " + node_option[i]);
+    }
+}
+function game_over(game) {
+    console.log("The game is over");
+    console.log("Do you want to play again? y/n");
+    var input = prompt("> ");
+    if (input === "y") { // input to start game
+        main(game);
+    }
+    else { } // else do nothing?
+}
+main(game_test);
