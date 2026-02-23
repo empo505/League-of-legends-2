006a189eb232b21b076a3547820af5f250334131
alax3723 <alva.m.axelsson@gmail.com>
2026-02-23 17:02:39 +0100

    installing vue

---
+++ vue-project/src/components/start_button.vue
@@ -0,0 +1,18 @@
+<template>
+    <button class="btn">
+        <slot></slot>
+    </button>
+</template>
+
+<style scoped>
+.btn {
+    background: pink;
+    color: black;
+    border: none;
+    cursor: pointer;
+    text-align: center;
+    font-size: large;
+    display: block;
+    margin: 0 auto;
+}
+</style>
