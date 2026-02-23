e7febc01416bda45b9fdc268d9c7dd6b99082a16
empo505 <eemeli-johannes.peura.7887@student.uu.se>
2026-02-23 17:02:11 +0100

    Test project with vue

---
+++ graphics/src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+  import Cbutton from "./components/Cbutton.vue";
+  import {ref} from "vue";'
+
+  const penis = ref(true)
+
+  function peniskoll(){
+    penis.value = !penis.value
+  }
+</script>
+
+<template>
+  <div>
+    <h1>Pool party Gragas
+    <Cbutton @click="peniskoll">Tryck för penis
+      </Cbutton>
+    <h1 v-if="penis"> PENIS </h1>
+    <h1 v-else> penis...? </h1>
+
+   </div>
+</template>
+
+
