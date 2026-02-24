cfa5c5cc588d17e5802f5d338ae2f2ad6261b165
alax3723 <alva.m.axelsson@gmail.com>
2026-02-24 17:57:56 +0100

    start page

---
+++ game-project/src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+//import {ref} from 'vue'
+import GameHeader from '@/components/header.vue'
+import GameMain from '@/components/main.vue'
+
+</script>
+
+<template>
+  <GameHeader/>
+  <GameMain/>
+</template>
+
+<style scoped>
+:global(body){
+  background-color: burlywood;
+}
+</style>
---
+++ game-project/src/components/header.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+    import {ref} from 'vue'
+    const titel = ref('Welcome to Game')
+</script>
+
+<template>
+  <header>
+    <span>{{ titel }}</span>
+  </header>
+</template>
+
+<style scoped>
+header span{
+  font-size: xx-large;
+  text-align: center;
+  font-weight: bold;
+  display: block;
+  margin: 0 auto;
+}
+</style>
